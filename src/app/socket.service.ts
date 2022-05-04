825f831c79520ca31c61e7ac8e7ceb36ff5be23a
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-05-04 12:44:41 +0300

    chat service

--- src/app/socket.service.ts
+++ src/app/socket.service.ts
@@ -1,8 +1,7 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, Observer} from "rxjs";
 import {environment} from "../environments/environment";
-import { io, Socket} from "socket.io-client";
-import {DefaultEventsMap} from '@socket.io/component-emitter';
+import {io} from "socket.io-client";
 
 @Injectable({
   providedIn: 'root'
@@ -10,27 +9,27 @@ import {DefaultEventsMap} from '@socket.io/component-emitter';
 export class SocketService {
 
   private url = environment.socketURL;
-  private socket: Socket<DefaultEventsMap, DefaultEventsMap> | undefined;
+  private socket = io(this.url, {
+    extraHeaders: {'Authorization': "Bearer " + localStorage.getItem("jwtToken")}
+  });
 
   constructor() {
 
   }
 
   public joinChannel() {
-    this.socket = io(this.url, {
-      extraHeaders: { 'Authorization' : "Bearer " + localStorage.getItem("jwtToken")}
-      // auth: {
-      //   token: (cb: (arg0: { token: any; }) => void) => {
-      //     cb({ token: localStorage['jwtToken'] })
-      // }}
-    });
+    let unreadMessages: any[];
+    return new Observable((observer: Observer<any>) => {
+      this.socket.on("unread_messages", (messages) => {
+        unreadMessages = messages;
+      })
+      observer.next(unreadMessages);
+      observer.complete();
+    })
   }
 
-
-  public logConnection = () => {
+  public logConnection() {
     return new Observable((observer: Observer<any>) => {
-
-      // @ts-ignore
       this.socket.on('connect', () => {
         console.log('The client has connected with the server. Username: ' + localStorage.getItem('username'));
         observer.next('connect');
@@ -38,4 +37,46 @@ export class SocketService {
       });
     });
   }
+
+  public sendMessage(username: string, message: string) {
+    console.log('Sending: ' + message);
+    const content = {
+      user_id: username,
+      message: message,
+    };
+    this.socket.emit('send_message', content);
+  }
+
+  public onlineUsers() {
+    return new Observable((observer: Observer<any>) => {
+      this.socket.on('new_connection', (msg) => {
+        console.log("new connection msg: " + JSON.stringify(msg));
+        observer.next(msg);
+
+      });
+    });
+  }
+
+  public userDisconnects() {
+    return new Observable((observer: Observer<any>) => {
+      this.socket.on('user_disconnect', (user_id) => {
+        observer.next(user_id);
+        observer.complete();
+      });
+    });
+  }
+
+  public getMessage() {
+    return new Observable((observer: Observer<any>) => {
+      this.socket.on('receive_message', (incoming: { sender_id: string, data: string, sent_at: any }) => {
+        console.log("incoming msg: " + JSON.stringify(incoming));
+        observer.next(incoming);
+      });
+    });
+  }
+
+
 }
+
+
+
